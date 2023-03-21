5f7c035498dafbaed870d7d46d103304697f86e7
haliltuzcu <007canberk@gmail.com>
2023-03-21 23:30:04 +0300

    app

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,19 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   news: [],
-}
+  loading: false,
+  error: false,
+};
+
+export const getNews = createAsyncThunk(
+  "getNews", //? action types
+  //? async callback func
+
+  async () => {
+    const API_KEY = "3e3aecc97ca84357a4daadd4fe98b585";
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+
+    try {
+      const { data } = await axios(url);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 const newsSlice = createSlice({
   name: "news",
   initialState,
   reducers: {
     clearNews: (state) => {
-      state.news = []
+      state.news = [];
     },
   },
-})
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNews.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getNews.fulfilled, (state, action) => {
+        state.news = action.payload;
+        state.loading = false;
+      })
+      .addCase(getNews.rejected, (state, action) => {
+        state.loading = false;
+      });
+  },
+});
 
-export const { clearNews } = newsSlice.actions
+export const { clearNews } = newsSlice.actions;
 
-export default newsSlice.reducer
+export default newsSlice.reducer;
